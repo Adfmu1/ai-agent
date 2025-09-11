13737ee5bf64007598ff282af48a34f73498161c
Adfmu1 <adfmu3@gmail.com>
2025-09-11 13:49:49 +0200

    add --verbose argument handling

--- main.py
+++ main.py
@@ -2,21 +2,36 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     if len(sys.argv) > 1:
+        prompt = sys.argv[1]
+        
         load_dotenv()
         api_key = os.environ.get("GEMINI_API_KEY")
 
         client = genai.Client(api_key=api_key)
-    
-        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
         
-        print(response.text)
+        messages = [
+                types.Content(role="user", parts=[types.Part(text=prompt)])
+        ]
+    
+        if "--verbose" in sys.argv:
+            for prompt in messages:
+                print("User prompt:", prompt)
+                response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+                
+                print(response.text)
 
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
+                print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+                print("Response tokens:", response.usage_metadata.candidates_token_count)
+        else:
+            response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+            print(response.text)
+            
+            
     else:
         print("no prompt provided")
         sys.exit(1)
