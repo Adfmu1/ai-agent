dc0f3b93a72f1cd8ac8d23a4db66c81da80312e0
Adfmu1 <adfmu3@gmail.com>
2025-09-14 20:06:57 +0200

    fix issues with get_files_info and add error handling

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,16 +1,21 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    dir = os.path.join(directory, working_directory)
-    if not os.path.exists(dir):
+    dir = os.path.abspath(os.path.join(working_directory, directory))
+    if not dir.startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    elif not os.path.isdir(directory):
+    elif not os.path.isdir(dir):
         return f'Error: "{directory}" is not a directory'
 
-    ret_string = []
-
-    for file in os.listdir(dir):
-        file_path = dir + "/" + file
-        ret_string.append(f"- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
+    try:
+        ret_string = []
         
-    return "\n".join(ret_string)
+        ret_string.append(f"Result for {"current" if directory == "." else directory} directory:")
+
+        for file in os.listdir(dir):
+            file_path = dir + "/" + file
+            ret_string.append(f"- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
+            
+        return "\n".join(ret_string)
+    except Exception as e:
+        return f"Error listing files: {e}"
