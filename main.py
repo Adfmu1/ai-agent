d1ddbacefc7013a4de8e2782329c04609703f9d8
Adfmu1 <adfmu3@gmail.com>
2025-09-15 11:13:26 +0200

    add function declaration of get_files_info to ai and add system prompt to chat

--- main.py
+++ main.py
@@ -3,7 +3,29 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+config = types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)
 
 def main():
     if len(sys.argv) > 1:
@@ -21,15 +43,31 @@ def main():
         if "--verbose" in sys.argv:
             for prompt in messages:
                 print("User prompt:", prompt)
-                response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
                 
+                response = client.models.generate_content(
+                    model="gemini-2.0-flash-001", 
+                    contents=prompt,
+                    config=config
+                    )
+
                 print(response.text)
+                if response.function_calls != None:
+                    print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
+                    
 
                 print("Prompt tokens:", response.usage_metadata.prompt_token_count)
                 print("Response tokens:", response.usage_metadata.candidates_token_count)
         else:
-            response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+            response = client.models.generate_content(
+                model="gemini-2.0-flash-001", 
+                contents=messages,
+                config=config
+                )
+            
             print(response.text)
+            if response.function_calls != None:
+                print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
             
             
     else:
